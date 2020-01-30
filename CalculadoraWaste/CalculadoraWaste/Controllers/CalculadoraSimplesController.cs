a14aece8026840627ded08f1e11f0bba5ccd7ea1
Leonardo <leonardo-toneti@hotmail.com>
2020-01-30 00:18:17 -0300

    Inicio do projeto e parte da estrutura

---
+++ CalculadoraWaste/CalculadoraWaste/Controllers/CalculadoraSimplesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraWaste.Controllers
+{
+    public class CalculadoraSimplesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
